b7df76afcd0ca2a84d57b57339658b96d860e1e4
Mahtab Molaei <mahtabmj@gmail.com>
2025-07-04 20:33:17 +0330

    initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+height = (input("Please enter your height (in): "))
+while not(height.isnumeric()):
+    print("wrong input! please enter a number")
+    height = (input("Please enter your height (in): "))
+else:
+    weight = (input("Please enter your weight (lb): "))
+    while not(weight.isnumeric()):
+        print("wrong input! please enter a number")
+        weight = (input("Please enter your weight (lb): "))
+
+weight = float(weight)
+height = float(height)
+
+bmi = round(703* weight/height**2,1)
+print(f"your BMI is: {bmi}")
+
+if bmi<18.5:
+    print("under weight")
+elif bmi<25:
+    print("normal weight")
+elif bmi<30:
+    print("over weight")
+elif bmi<35:
+    print("obese weight")
+else:
+    print("clinically obese")
+
